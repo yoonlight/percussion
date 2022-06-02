3b750ced01ed332d4905fd6e1d4908cf9ea7f483
yoonlight <yoonlight12@gmail.com>
2022-06-02 20:10:32 +0900

    build: update build config

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/percussion/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
