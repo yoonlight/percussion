f462da99ece04679799e465ddabeb1157d5636a5
yoonlight <yoonlight12@gmail.com>
2022-06-02 19:37:15 +0900

    Initial commit

---
+++ src/components/RegisterDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn label="등록" color="primary" @click="alert = true" />
+
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          아직 멤버가 없어요 ㅜㅜ
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+
+      address: ref(''),
+    };
+  },
+};
+</script>
